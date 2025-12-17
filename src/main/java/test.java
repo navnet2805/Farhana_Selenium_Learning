0715cc2876e1a1dddeadcbca90f4fde5e2f3c25e
Naveen <naveen.rp.2805@gmail.com>
2025-12-17 21:42:22 +0530

    Commit 02

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class test {
+    public static void main(String[] args) {
+        ChromeDriver driver = new ChromeDriver(); // it will launch chrome browser.
+        driver.get("https://awesomeqa.com/hr/web/index.php/auth/login"); //it will enter the url
+        //driver.quit(); //this will close browser and quit session
+        driver.close(); //this will only close browser
+    }
 }
