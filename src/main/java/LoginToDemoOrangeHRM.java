e45ce2dbeb59458fd080d79644d958b3feaeb80b
Naveen <naveen.rp.2805@gmail.com>
2025-12-19 22:26:29 +0530

    Commit 03

---
+++ src/main/java/LoginToDemoOrangeHRM.java
@@ -0,0 +1,54 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class LoginToDemoOrangeHRM
+{
+    public static void main(String[] args) throws InterruptedException
+    {
+        WebDriver driver = null;
+
+        try
+        {
+            //launch browser
+            driver =new ChromeDriver();
+
+            //enter the URL
+            driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+            Thread.sleep(Duration.ofSeconds(2));
+
+            //find the element for username and enter the username
+            WebElement usernameText = driver.findElement(By.xpath("//input[@name='username']"));
+            usernameText.click();
+            usernameText.clear();
+            usernameText.sendKeys("Admin");
+
+            //find the element for password and enter the password
+            WebElement passwordText = driver.findElement(By.xpath("//input[@name='password']"));
+            passwordText.click();
+            passwordText.clear();
+            passwordText.sendKeys("admin123");
+
+            //find the element for login button and click on it.
+            WebElement loginButton = driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--main orangehrm-login-button']"));
+            loginButton.click();
+        }
+        catch (RuntimeException e) {
+            System.out.println("Exception is occured");
+        }
+        finally {
+            Thread.sleep(Duration.ofSeconds(5));
+            driver.quit();
+        }
+
+
+
+
+
+
+    }
+}
